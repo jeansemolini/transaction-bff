cb2049a6e570e8c817965f256638b4e9812aec68
Jean Semolini <jean.maiko@hotmail.com>
2022-06-07 09:56:35 -0300

    aula 09 - Spring Retry

--- src/main/java/br/com/coffeeandit/transactionbff/domain/TransactionService.java
+++ src/main/java/br/com/coffeeandit/transactionbff/domain/TransactionService.java
@@ -2,20 +2,24 @@ package br.com.coffeeandit.transactionbff.domain;
 
 import br.com.coffeeandit.transactionbff.dto.RequestTransactionDto;
 import br.com.coffeeandit.transactionbff.dto.TransactionDto;
-import br.com.coffeeandit.transactionbff.exception.UnauthorizedException;
 import br.com.coffeeandit.transactionbff.redis.TransactionRedisRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class TransactionService {
 
     private TransactionRedisRepository transactionRedisRepository;
+    private RetryTemplate retryTemplate;
 
-    public TransactionService(TransactionRedisRepository transactionRedisRepository) {
+    public TransactionService(TransactionRedisRepository transactionRedisRepository, RetryTemplate retryTemplate) {
         this.transactionRedisRepository = transactionRedisRepository;
+        this.retryTemplate = retryTemplate;
     }
 
     public Optional<TransactionDto> save(final RequestTransactionDto requestTransactionDto) {
@@ -23,7 +27,17 @@ public class TransactionService {
         return Optional.of(transactionRedisRepository.save(requestTransactionDto));
     }
 
+//    Retry utilizando anotação
+//    @Retryable(value = QueryTimeoutException.class, maxAttempts = 5, backoff = @Backoff(delay = 1000))
+//    public Optional<TransactionDto> findbyId(final String id) {
+//        log.info("Consultando Redis...");
+//        return transactionRedisRepository.findById(id);
+//    }
+
     public Optional<TransactionDto> findbyId(final String id) {
-        return transactionRedisRepository.findById(id);
+        return retryTemplate.execute(ret -> {
+            log.info("Consultando Redis...");
+            return transactionRedisRepository.findById(id);
+        });
     }
 }

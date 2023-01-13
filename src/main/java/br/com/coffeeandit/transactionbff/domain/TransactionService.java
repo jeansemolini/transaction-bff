b55a014792c7985f06d2622088d837ba3ca3e50c
Jean Semolini <jean.maiko@hotmail.com>
2023-01-12 21:11:34 -0300

    aula 11 - Spring Kafka Producer

--- src/main/java/br/com/coffeeandit/transactionbff/domain/TransactionService.java
+++ src/main/java/br/com/coffeeandit/transactionbff/domain/TransactionService.java
@@ -4,8 +4,14 @@ import br.com.coffeeandit.transactionbff.dto.RequestTransactionDto;
 import br.com.coffeeandit.transactionbff.dto.TransactionDto;
 import br.com.coffeeandit.transactionbff.redis.TransactionRedisRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.QueryTimeoutException;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -14,16 +20,27 @@ import java.util.Optional;
 @Service
 public class TransactionService {
 
-    private TransactionRedisRepository transactionRedisRepository;
-    private RetryTemplate retryTemplate;
+    private final TransactionRedisRepository transactionRedisRepository;
+    private final RetryTemplate retryTemplate;
+    private final ReactiveKafkaProducerTemplate<String, RequestTransactionDto> reactiveKafkaProducerTemplate;
 
-    public TransactionService(TransactionRedisRepository transactionRedisRepository, RetryTemplate retryTemplate) {
+    @Value("${app.topic}")
+    private String topic;
+
+    public TransactionService(TransactionRedisRepository transactionRedisRepository, RetryTemplate retryTemplate,
+                              ReactiveKafkaProducerTemplate<String, RequestTransactionDto> reactiveKafkaProducerTemplate) {
         this.transactionRedisRepository = transactionRedisRepository;
         this.retryTemplate = retryTemplate;
+        this.reactiveKafkaProducerTemplate = reactiveKafkaProducerTemplate;
     }
 
+    @Transactional
+    @Retryable(value = QueryTimeoutException.class, maxAttempts = 5, backoff = @Backoff(delay = 100))
     public Optional<TransactionDto> save(final RequestTransactionDto requestTransactionDto) {
         requestTransactionDto.setData(LocalDateTime.now());
+        reactiveKafkaProducerTemplate.send(topic, requestTransactionDto)
+                .doOnSuccess(voidSenderResult -> log.info(voidSenderResult.toString()))
+                .subscribe();
         return Optional.of(transactionRedisRepository.save(requestTransactionDto));
     }
 
